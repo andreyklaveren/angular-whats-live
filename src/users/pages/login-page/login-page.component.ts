d3baceff768a0b3a78c9448b009e58283da156cf
andreyklaveren <andreyklaveren@gmail.com>
2024-03-03 10:58:35 -0300

    primeira alt

---
+++ src/users/pages/login-page/login-page.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  template: `
+    <div class="container">
+      <div>Escolha um Login</div>
+      <input type="file" #input>
+
+      <div class="user">
+        <img src="">
+        <span>Nome usuario 1</span>
+        <button (click)="input.click()">im</button>
+      </div>
+      <div class="user">
+        <img src="">
+        <span>Nome usuario 2</span>
+        <button (click)="input.click()">im</button>
+      </div>
+      <div class="user">
+        <img src="">
+        <span>Nome usuario 3</span>
+        <button (click)="input.click()">im</button>
+      </div>
+    </div>
+  `,
+  styleUrl: './login-page.component.scss'
+})
+export class LoginPageComponent {
+
+}
